c892baa97c943de0253c07d2a41db70f236f47f5
Mostafa Ahangarha <ahangarha@riseup.net>
2022-05-30 18:47:54 +0300

    use nested resources for users and posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/:user_id/posts', to: 'posts#index'
-  get 'users/:user_id/posts/:post_id', to: 'posts#show'
-  get 'users', to: 'users#index'
-  get 'users/:user_id', to: 'users#show'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
