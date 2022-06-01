428c958859f27e284eac5dc7c1e67df471a6ac3d
Mostafa Ahangarha <ahangarha@riseup.net>
2022-06-01 20:12:50 +0300

    fix the wrong name for like migration

--- db/schema.rb
+++ db/schema.rb
@@ -24,13 +24,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_31_133344) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "links", force: :cascade do |t|
+  create_table "likes", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_links_on_post_id"
-    t.index ["user_id"], name: "index_links_on_user_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -55,7 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_31_133344) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
-  add_foreign_key "links", "posts"
-  add_foreign_key "links", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
