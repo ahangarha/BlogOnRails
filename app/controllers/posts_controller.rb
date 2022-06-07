7092e3e027903c8ded4d032bac5ef1647ed5868e
Mostafa Ahangarha <ahangarha@riseup.net>
2022-06-07 14:11:49 +0300

    add form for new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,26 @@ class PostsController < ApplicationController
     @posts = Post.where(user: params[:user_id])
   end
 
+  def create
+    post = params[:post]
+    user = current_user
+
+    new_post = Post.new(post.permit(:title, :text))
+    new_post.user = user
+
+    if new_post.save
+      redirect_to user_post_url(user, new_post)
+    else
+      @post = new_post
+      render :new
+    end
+  end
+
+  def new
+    @post = Post.new
+    render :new
+  end
+
   def show
     @post = Post.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :create, :new, :show]
   end
 
   root to: 'users#index'
