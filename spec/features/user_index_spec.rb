c0e2547169238553b97383b6b74a248674076b1a
Mostafa Ahangarha <ahangarha@riseup.net>
2022-06-16 00:06:51 +0300

    refactor and extract model creator methods to helper

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -1,12 +1,5 @@
 require 'rails_helper'
-
-def create_and_activate_user(name)
-  user = User.create!(name:, email: "#{name}@example.com", password: '123456')
-  user.confirm
-  user.photo = "#{name}.jpg"
-  user.save
-  user
-end
+require 'helpers/models_helper'
 
 feature 'User Index' do
   background do
